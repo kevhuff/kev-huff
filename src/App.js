491d3251ed1acf4fc909af44636c3269da70f667
kevhuff <kevinhuffman23@gmail.com>
2023-09-17 23:32:57 -0400

    update defauly routing and fallback error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Hero from './components/hero';
 import Contact from './components/contact'; 
@@ -10,20 +9,23 @@ import Projects from './components/projects';
 import Resume from './components/resume';
 import About from './components/About';
 import StarBackground from './components/StarBackground';
+import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}> 
       <div>
         <StarBackground />  {/* This renders the starry background */}
         <NavBar />
         <Routes>
-          <Route path="/" element={<Hero />} />
+          <Route path="/" element={<Hero />} exact /> 
           <Route path="/contact" element={<Contact />} />
           <Route path="/socials" element={<SocialMediaSection />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/about" element={<About />} />
+          <Route path="*" element={<Redirect to="/" />} /> {/* This will redirect any unknown route back to "/" */}
         </Routes>
         <ScrollToTopButton />
         <Footer />
@@ -31,6 +33,5 @@ function App() {
     </Router>
   );
 }
-
 
 export default App;
