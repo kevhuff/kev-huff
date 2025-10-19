c75fa96c8ccf3952d6cc9de27b842ca34b4acf04
kevhuff <kevinhuffman23@gmail.com>
2025-10-19 13:22:35 -0400

    deleting old resume and updated paths

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,19 +1,9 @@
-
 import React from 'react';
-import resumePreview from './assets/respreview.png';
-import resumePDF from './assets/Kevin-Huffman.pdf';
-const Resume = () => {
-    
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen mb-10">
-            <h2 className="text-5xl font-bold mb-6 text-indigo-200">My Resume</h2>
-            <img src={resumePreview} alt="Resume Preview" className="w-[40%] h-[40%] mx-auto mb-4 rounded" />
-            <div className="text-center">
-                <a href={resumePDF} download="Your_Name_Resume.pdf" className="mt-4 bg-indigo-600 px-8 py-2 text-white rounded-full hover:bg-indigo-700 transition ease-in-out duration-150">
-                    Download as PDF
-                </a>
-            </div>
-        </div>
-    );
-};
-    export default Resume;
+
+// pick ONE preview image you actually want to show:
+import resumePreview from './assets/resume_prev_mech.png';
+// or: import resumePreview from './assets/res_ats.png';
+// or: import resumePreview from './assets/H2preview.png';
+
+import mechanicResumePdf from './assets/Kevin_Huffman_Mechanic_Resume.pdf';
+import atsResumePdf from './assets/Kevin_Huffman_Resume_ATS-1.pdf';
